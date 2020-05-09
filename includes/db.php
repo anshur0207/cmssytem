4884cb76c462811cccccf9d60a53943cf088de09
anshur0207 <anshur0202@gmail.com>
2020-05-09 23:15:17 +0530

    my second commit

---
+++ includes/db.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$connection =mysqli_connect('localhost','root','','cms');
+//
+//if($connection){
+//    echo "we are Connected";
+//}
+//else{
+//    echo "no connection";
+//}
+
+
+
+
+
+
+
+
+
+?>
---
+++ search.php
@@ -0,0 +1,134 @@
+<?php include "includes/db.php"; ?>
+<?php include "includes/header.php"; ?>
+  
+  <?php include "includes/navigation.php"; ?>
+
+  
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+            
+      
+            
+
+            <!-- Blog Entries Column -->
+            <div class="col-md-8">
+            <?php
+                
+                
+     if(isset($_POST['submit'])){
+     
+     $search= $_POST['search'];
+     }
+     
+     $query= "SELECT * FROM posts WHERE post_tags LIKE '%$search%'";
+     $search_query= mysqli_query($connection,$query);
+     
+     if(!$search_query){
+         die("QUERY FAILED".mysqli_error($_connection));
+     }
+     
+     $count = mysqli_num_rows($search_query);
+     
+     if($count == 0){
+         
+         echo "No Results";
+     }
+     else
+         echo " Some Results";
+     
+                
+                
+                
+                
+                
+                
+                
+                
+            
+            $query = "SELECT * FROM posts";
+//LIMIT $page_1, $per_page";
+        $select_all_posts_query = mysqli_query($connection,$query);
+
+        while($row = mysqli_fetch_assoc($select_all_posts_query)) {
+        $post_id = $row['post_id'];
+        $post_title = $row['post_title'];
+        $post_author = $row['post_author'];
+        $post_date = $row['post_date'];
+        $post_image = $row['post_image'];
+        $post_content = substr($row['post_content'],0,400);
+        $post_status = $row['post_status'];
+        
+
+    
+        ?>
+                <h1 class="page-header">
+                    Page Heading
+                    <small>Secondary Text</small>
+                </h1>
+
+                <!-- First Blog Post -->
+               <h2>
+                    <a href="post/<?php echo $post_id; ?>"><?php echo $post_title ?></a>
+                </h2>
+                <p class="lead">
+                    by <a href="author_posts.php?author=<?php echo $post_author ?>&p_id=<?php echo $post_id; ?>"><?php echo $post_author ?></a>
+                </p>
+                <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
+                <hr>
+                
+                
+                <a href="post.php?p_id=<?php echo $post_id; ?>">
+                <img class="img-responsive" src="images/<?php echo $post_image;?>" alt="">
+                </a>
+                
+                
+                
+                <hr>
+                <p><?php echo $post_content ?></p>
+                <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                <hr>
+                
+
+   <?php } 
+                
+                }
+                
+                
+                }
+                
+                
+                
+                ?>
+
+                
+                
+                
+                
+                
+
+              
+    
+
+            </div>
+            
+            <!-- Blog Sidebar Widgets Column -->
+           <?php 
+            
+            
+            include "includes/side.php"
+            
+            
+            ?>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+  <?php
+        
+  include "includes/footer.php"       
+?>        
